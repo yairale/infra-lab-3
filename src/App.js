24dd2c540ff25cbd70f244cc035e34e29dfc73ee
yairale <yair.ale@educacionit.com>
2023-04-26 19:58:39 -0300

    mensaje

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import gioconda from './gioconda.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Se actualizó el 25/04/2023
+        </p>
+        <img src={gioconda}/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
